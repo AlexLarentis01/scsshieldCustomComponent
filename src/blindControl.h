0c5096af25887954ad8970509127480bfe039f7d
Alex Larentis <alexlarentis@icloud.com>
2022-06-03 14:08:11 +0200

    setup custom esphome component

---
+++ src/blindControl.h
@@ -0,0 +1,18 @@
+#include "esphome.h"
+#include "Interfaccia.h" 
+
+class MyCustomComponent : public Component {
+ public:
+  void setup() override {
+    // Setup the component
+
+
+
+  }
+  void loop() override {
+    // This is called every loop
+
+
+    
+  }
+};
